8c60a6444cc6a55c0da9f93138678aaa374142be
David Castro <jdcastro0667@misena.edu.co>
2020-08-30 09:43:57 -0500

    cambios

--- editar.php
+++ editar.php
@@ -5,7 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/styles.css">
 		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-
+		<link rel="icon" href="data:;base64,iVBORw0KGgo=">
 		<script src="js/jquery.min.js"></script>
 
 	</head>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="bootstrap/css/styles.css">
 		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-		<link rel="shortcut icon" href="">
+		<link rel="icon" href="data:;base64,iVBORw0KGgo=">
 	</head>
 	<body>
 	<!-- Fin del head -->
